0aac7af02acd645966b935597e813c9c258b5af5
Michelle <mntomlin1@gmail.com>
2022-09-01 18:18:06 -0400

    mapProperties

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,12 +1,39 @@
 const knex = require("../db/connection");
 
+//below is to configure the properties for the joined table "categories" to be nested object and used by read
+const mapProperties = require("../utils/map-properties");
+
+const addCategory = mapProperties({
+category_id: "category.category_id",
+category_name: "category.category_name",
+category_description: "category.category_description",
+});
+
+//begin queries for the product service
+
 function list() {
   return knex("products").select("*");
 }
 
+/*basic read productId before joining read with categories in query builder below
 function read(productId) {
   return knex("products").select("*").where({ product_id: productId }).first();
 }
+*/
+
+function read(product_id) {
+  return knex("products as p")
+    .join("products_categories as pc", "p.product_id", "pc.product_id")
+    .join("categories as c", "pc.category_id", "c.category_id")
+    .select("p.*", "c.*")
+    .where({ "p.product_id": product_id })
+    .first()
+    .then (addCategory);
+}
+
+
+
+
 
 //add new query builder function to the service
 
@@ -17,6 +44,7 @@ function listOutOfStockCount() {
     .where({ product_quantity_in_stock: 0 })
     .groupBy("out_of_stock");
 }
+
 
 //add new query builder function to the service
 
@@ -43,6 +71,10 @@ function listTotalWeightByProduct() {
     )
     .groupBy("product_title", "product_sku");
 }
+
+
+//new query builder to join products and categories table
+
 
 
 module.exports = {
